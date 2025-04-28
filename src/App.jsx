b61cd4c1afbfbeda7a3d8e25a9d690d523e0e835
lekoooooo <levdidebashvili@gmail.com>
2025-04-28 05:53:24 +0400

    feature
    fixed positions of divs and img

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,77 @@
-import React from "react";
-
 const App = () => {
   return (
-    <div className="flex flex-col w-full gap-[48px] max-w-[385px] text-center">
-      <div>
-        {" "}
-        <h2 className="text-[32px] font-semibold m-auto max-w-[270px]">
+    <div className="flex flex-col w-full max-w-[1440px] mx-auto px-4 lg:px-8 gap-8">
+      <div className="flex flex-col m-auto  text-center lg:text-left lg:items-start">
+        <h2 className="text-[32px] font-semibold max-w-[490px]">
           Travel to make sweet memories
         </h2>
-        <p className="font-light text-gray-900">
-          Find trips that fit your schedule
+        <p className="font-light text-gray-900 m-auto mt-4">
+          Find the perfect vacation getaway
         </p>
-        <img
-          src="./swimming.png"
-          className="pt-[48px] m-auto"
-          alt="good times"
-        />
       </div>
-      <div className="flex flex-col items-start pl-[20px]">
-        <div className=" pt-[64px]  ">
-          <img src="./1.svg" className="pb-[18px] w-[43px]" alt="numero uno" />
-          <h2 className="text-[24px] font-semibold max-w-[230px] text-start mb-[18px]">
-            Reignite those travel tastebuds
-          </h2>
-          <p className="text-[14px] font-light text-gray-900 text-start max-w-[280px]">
-            Embark on a gastronomic adventure that transports you to distant
-            lands, where every dish tells a story.
-          </p>
+
+      <div className="flex flex-col-reverse lg:flex-row gap-12 items-center">
+        <div className="flex-1 flex flex-col items-start">
+          <div className="flex flex-col gap-8 mt-4 ">
+            <div className="flex items-start gap-4">
+              <div className="w-[54px] h-[36px] bg-[#18ABC6] text-white flex items-center justify-center rounded-xl font-semibold">
+                1
+              </div>
+              <div>
+                <h3 className="text-[20px] font-semibold">
+                  Reignite those travel tastebuds
+                </h3>
+                <p className="text-[14px] font-light text-gray-900 mt-2 max-w-[340px]">
+                  Embark on a gastronomic adventure that transports you to
+                  distant lands, where every dish tells a story.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <div className="w-[54px] h-[36px] bg-[#18ABC6] text-white flex items-center justify-center rounded-xl font-semibold">
+                2
+              </div>
+              <div>
+                <h3 className="text-[20px] font-semibold">
+                  Get back to nature by travel
+                </h3>
+                <p className="text-[14px] font-light text-gray-900 mt-2 max-w-[340px]">
+                  Escape the hustle and bustle of daily life and reconnect with
+                  the serene beauty of the natural.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <div className="w-[54px] h-[36px] bg-[#18ABC6] text-white flex items-center justify-center rounded-xl font-semibold">
+                3
+              </div>
+              <div>
+                <h3 className="text-[20px] font-semibold">
+                  Find trips that fit your freedom
+                </h3>
+                <p className="text-[14px] font-light text-gray-900 mt-2 max-w-[340px]">
+                  Embark on a gastronomic adventure that transports you to
+                  distant lands, where every dish tells a story.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <button className="mt-12 bg-[#18ABC6] hover:bg-amber-600 text-white py-2 px-6 rounded-[8px] max-w-[200px]">
+            Start your explore
+          </button>
         </div>
-        <div className=" pt-[64px] ">
-          <img src="./2.svg" className="pb-[18px] w-[43px]" alt="numero uno" />
-          <h2 className="text-[24px] font-semibold mb-[18px] max-w-[230px] text-start">
-            Get back to nature by travel
-          </h2>
-          <p className="text-[14px] font-light text-gray-900 text-start max-w-[270px]">
-            Escape the hustle and bustle of daily life and reconnect with the
-            serene beauty of the natural.
-          </p>
-        </div>
-        <div className="pt-[64px]">
-          <img src="./3.svg" className="pb-[18px] w-[43px]" alt="numero uno" />
-          <h2 className="text-[24px] font-semibold  mb-[18px] max-w-[230px] text-start">
-            Find trips that fit your freedom
-          </h2>
-          <p className="text-[14px] font-light text-gray-900 text-start max-w-[280px]">
-            Embark on a gastronomic adventure that transports you to distant
-            lands, where every dish tells a story.
-          </p>
+
+        <div className="flex-1 flex justify-center relative">
+          <img
+            src="./swimming.png"
+            className="rounded-[20px] lg:max-w-[500px] lg:h-[613px] w-full object-cover"
+            alt="swimming"
+          />
         </div>
       </div>
-      <button
-        className=" w-[230px]
-        bg-[#18ABC6]
-        hover:bg-amber-600
-        cursor-pointer
-       py-2 
-       m-auto
-      
-      text-white
-      text-center
-      rounded-[8px]"
-      >
-        Start your explore
-      </button>
     </div>
   );
 };
